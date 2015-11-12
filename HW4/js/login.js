56fe159df22439c990e283d02a0d0349c932ec00
Christine Lao <clao@ucsd.edu>
2015-11-12 10:09:49 -0800

    Sign Up Sucessful animation

--- HW4/js/login.js
+++ HW4/js/login.js
@@ -1,4 +1,10 @@
 function onClickSignUp() {
   var signUpText = document.getElementById("signInMessage");
-  signUpText.style.display = "block";
+
+  var buttons = document.getElementsByClassName('buttons')[0]; 
+  buttons.style.position = 'relative'; 
+  $(buttons).animate({top: '65px'}, 'slow', function(){
+  	  signUpText.style.display = "block";
+  	  buttons.style.top = 0; 
+  }); 
 }
