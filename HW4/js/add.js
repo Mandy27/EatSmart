4f9e541b4cf453548b56b27a6ab16f4937427132
Mandy27 <giaman27@gmail.com>
2015-11-17 12:30:28 -0800

    Fixed

--- HW4/js/add.js
+++ HW4/js/add.js
@@ -1,4 +1,21 @@
 function toggleReminder(){
-	$(".reminderDiv").toggleClass("reminderDivShow");
+	$(".hide").toggleClass("reminderDivShow");
 	$(".fa-chevron-right").toggleClass("fa-chevron-down");
+}
+
+function alldayCheckbox(){
+	if ($('#allday').prop('checked')) {
+		var temp = $('.timepicker');
+		for(var i = 0; i < temp.length; i++){
+			$('.timepicker')[i].readOnly = true;
+		}
+		$('.text-format').addClass('hideTimepicker');
+	}
+	else {
+		var temp = $('.timepicker');
+		for(var i = 0; i < temp.length; i++){
+			$('.timepicker')[i].readOnly = false;
+		}
+		$('.text-format').removeClass('hideTimepicker');
+	}
 }
