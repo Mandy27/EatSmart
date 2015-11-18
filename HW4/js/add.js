38f711a739f3a6bb543a8cff761980d7c05f566c
Harrison Wang <harrisonwang@Harrisons-MacBook-Pro-4.local>
2015-11-17 20:41:56 -0800

    implement firebase

--- HW4/js/add.js
+++ HW4/js/add.js
@@ -2,3 +2,21 @@ function toggleReminder(){
 	$(".reminderDiv").toggleClass("reminderDivShow");
 	$(".fa-chevron-right").toggleClass("fa-chevron-down");
 }
+
+//when "save it" is clicked
+function addHabit(){
+	//return an error when habit title is not correctly filled out
+	var x = document.forms["myForm"]["habit"].value;
+		if (x == "" | x == null  ){
+			alert("Habit must be added!");
+			return false; 
+		}
+
+	//add the habit to the list	(Firebase?)
+}
+
+//navigate to the list (Firebase?)
+function habitIconClick(){
+}
+
+
