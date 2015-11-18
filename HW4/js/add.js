b384760b61b05b08c20c9c5246cd9524b264f1d5
Harrison Wang <harrisonwang@Harrisons-MacBook-Pro-4.local>
2015-11-17 20:48:07 -0800

    firebase

--- HW4/js/add.js
+++ HW4/js/add.js
@@ -4,22 +4,6 @@ function toggleReminder(){
 }
 
 <<<<<<< HEAD
-//when "save it" is clicked
-function addHabit(){
-	//return an error when habit title is not correctly filled out
-	var x = document.forms["myForm"]["habit"].value;
-		if (x == "" | x == null  ){
-			alert("Habit must be added!");
-			return false; 
-		}
-
-	//add the habit to the list	(Firebase?)
-}
-
-//navigate to the list (Firebase?)
-function habitIconClick(){
-}
-
 
 =======
 // function alldayCheckbox(){
