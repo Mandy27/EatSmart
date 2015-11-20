a98ced6d8d4df6a3f48731bded79bc6afa621cfb
Chloe Lopez <cml036@ucsd.edu>
2015-11-19 23:49:40 -0800

    Cleaned up some code in list.js, list.html, forms.css. Removed firebase.json and listextra.html.

--- HW4/js/list.js
+++ HW4/js/list.js
@@ -1,34 +1,5 @@
-/*global $ */
-
-/* Sortable list items */
-//var el = document.getElementById('habit-list');
-//var sortable = Sortable.create(el);
-/*var dataRef = new Firebase('https://burning-heat-9490.firebaseio.com/Habits');
-
-var habitList = $('#habit-list');
-//var habitInfo = $('#habit-info');*/
-
-/* Fetch habit list data */
-/*dataRef.on('child_added', function (snapshot) {
-    // get data
-    var data = snapshot.val();
-    var title = data.title;
-    
-    console.log(title);
-    
-    // create elements
-    var habitElement = $("<li>");
-    var titleElement = $("<div class='habit-name'></div>");
-    
-    titleElement.text(title);
-    habitElement.text("message").prepend(titleElement);
-    
-    // add habit
-    habitList.html(habitElement);
-});*/
-
 /* Display habit list */
-/*$(window).load(function () {
+$(window).load(function () {
     var habitList = document.getElementById('habit-list').children;
 
     for (var i = 0; i < habitList.length; i++){
@@ -39,15 +10,15 @@ var habitList = $('#habit-list');
 
     // Add transition
     fadeAllIn(); 
-});*/
+});
 
-/* function displayHabits (name, text) {
+function displayHabits (name, text) {
     $('<div/>').text(text).prepend($('<em/>').text(title+': ')).appendTo($('#messagesDiv'));
     $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-}; */
+};
 
 /* Fade-in transition */
-/*function fadeAllIn() {
+function fadeAllIn() {
     var habitList = document.getElementById('habit-list').children;
     var delay = 0;
 
@@ -56,42 +27,39 @@ var habitList = $('#habit-list');
         $(habitList[i]).delay(delay).animate({right: '0px'}, 750); 
         delay+=100; 
     }
-}*/
+}
 
 function updateProgress(element, int) {
-     var msgElement = (element.parentNode.parentNode.getElementsByClassName("message-today"))[0];
-     //alert(msgElement.innerHTML);
-     $(msgElement).animate({opacity: 1}, 300); 
-     //msgElement.style.visibility="visible";
+    var msgElement = (element.parentNode.parentNode.getElementsByClassName("message-today"))[0];
+    $(msgElement).animate({opacity: 1}, 300); 
 
-     //Progress bar animation 
-     var progress = element.parentNode.parentNode.getElementsByClassName('progress');
-     var oldx2 = progress[0].getAttribute('x2'); 
+    //Progress bar animation 
+    var progress = element.parentNode.parentNode.getElementsByClassName('progress');
+    var oldx2 = progress[0].getAttribute('x2'); 
 
-     var bar = element.parentNode.parentNode.getElementsByClassName('bar');
-     var oldx1 = bar[0].getAttribute('x1'); 
+    var bar = element.parentNode.parentNode.getElementsByClassName('bar');
+    var oldx1 = bar[0].getAttribute('x1'); 
     
-     int++;
-    console.log(int);
+    int++;
 
-     // Animates from current location on the bar 
-     $({x2:oldx2}).animate(
-         {x2: (Number(oldx2) + 5) + 'px'},
-         {
-             duration:200,
-             step:function(now){$(progress).attr('x2', now);
-             queue: false; 
-         }
-     });
+    // Animates from current location on the bar 
+    $({x2:oldx2}).animate(
+        {x2: (Number(oldx2) + 5) + 'px'},
+        {
+            duration:200,
+            step:function(now){$(progress).attr('x2', now);
+            queue: false; 
+        }
+    });
 
-     $({x1:oldx1}).animate(
-         {x1: (Number(oldx1) + 5) + 'px'},
-         {
-             duration:200,
-             step:function(now){$(bar).attr('x1', now);
-             queue: false; 
-         }
-     });
+    $({x1:oldx1}).animate(
+        {x1: (Number(oldx1) + 5) + 'px'},
+        {
+            duration:200,
+            step:function(now){$(bar).attr('x1', now);
+            queue: false; 
+        }
+    });
 
     //keep track of clicks 
     var incrementProgress = new Firebase('https://burning-heat-9490.firebaseio.com/');
@@ -106,15 +74,16 @@ function updateProgress(element, int) {
         incrementProgress.child('counter').transaction(function(currentValue){
             return (currentValue || 0) + 1
         }, function(err, committed, ss){
-                if(err){
-                    setError(err);
-                }
-                else if(committed){
-                    //if update succeeds, then create a record
-                    addRecord(ss.val());
-                }
-            });
-        }
+            if(err){
+                setError(err);
+            }
+            
+            else if(committed){
+                //if update succeeds, then create a record
+                addRecord(ss.val());
+            }
+        });
+    }
 
     //create new incremental record
     function addRecord(id) {
@@ -126,6 +95,7 @@ function updateProgress(element, int) {
     }
 }
 
+/* Transition animations when a habit is deleted */
 function deleteHabit(element) {
     var child = element.parentNode.parentNode;
     var parent = child.parentNode;
@@ -134,23 +104,7 @@ function deleteHabit(element) {
      $(child).closest('li').slideUp('slow', function(){
          $(child).remove(); 
      }); 
-
-     //Fades out the child to delete
-     /*$(child).fadeOut(500, function(){ $(this).remove();});
-
-         $({x1:oldx1}).animate(
-             {x1: (Number(oldx1) + 5) + 'px'},
-             {
-                 duration:200,
-                 step:function(now){$(bar).attr('x1', now);
-                 queue: false; 
-             }
-         });*/
 }
-
-/*function deleteHabit (dataRef, snapshot) {
-    dataRef.remove();
-}*/
 
 // keep track of habit progress 
 var errorId = 0;
@@ -182,43 +136,49 @@ function incrementId(){
      });
  }*/
 
- function editPageTransition(){
-     var habitList = document.getElementById('habit-list').children;
-     var delay = 0; 
-     //var n = child.nextElementSibling;
-     console.log(habitList); 
-     for (var i = 0; i < habitList.length; i++){
-         console.log(habitList[i]); 
-          //Slides to the left of the screen to delete 
-         habitList[i].style.position = 'relative'; 
-         if (i != habitList.length - 1){
-             $(habitList[i]).delay(delay).animate({right: '1500px'}, 500); 
-             delay+=100; 
-         }
-         else{
-             $(habitList[i]).delay(delay).animate({right: '1500px'}, 500, function(){window.location.href='edit.html'}); 
-         }
-     }
- }
+/* Transition animations on navigating to the edit page */
+function editPageTransition(){
+    var habitList = document.getElementById('habit-list').children;
+    var delay = 0; 
+    //var n = child.nextElementSibling;
+    for (var i = 0; i < habitList.length; i++){
+        console.log(habitList[i]); 
+        
+        //Slides to the left of the screen to delete 
+        habitList[i].style.position = 'relative'; 
+        
+        if (i != habitList.length - 1){
+            $(habitList[i]).delay(delay).animate({right: '1500px'}, 500); 
+            delay+=100; 
+        }
+        
+        else{
+            $(habitList[i]).delay(delay).animate({right: '1500px'}, 500, function(){window.location.href='edit.html'}); 
+        }
+    }
+}
 
- function addPageTransition(){
-     var habitList = document.getElementById('habit-list').children;
-     var delay = 0; 
-     for (var i = 0; i < habitList.length; i++){
-         habitList[i].style.position = 'relative'; 
-         if (i != habitList.length - 1){
-             $(habitList[i]).delay(delay).animate({right: '1500px'}, 500); 
-             delay+=100; 
-         }
-         else{
-             $(habitList[i]).delay(delay).animate({right: '1500px'}, 500, function(){window.location.href='add.html'}); 
-         }
-     }
+/* Transition animations on navigating to the add page */
+function addPageTransition(){
+    var habitList = document.getElementById('habit-list').children;
+    var delay = 0; 
+    for (var i = 0; i < habitList.length; i++){
+        habitList[i].style.position = 'relative'; 
+        if (i != habitList.length - 1){
+            $(habitList[i]).delay(delay).animate({right: '1500px'}, 500); 
+            delay+=100; 
+        }
+        
+        else{
+            $(habitList[i]).delay(delay).animate({right: '1500px'}, 500, function(){window.location.href='add.html'}); 
+        }
+    }
 
-     if (habitList.length == 0){
-         window.location.href='add.html'; 
-     }
- }
+    if (habitList.length == 0){
+        window.location.href='add.html'; 
+    }
+
+}
 
 
 
